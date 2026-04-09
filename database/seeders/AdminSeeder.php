0fd4eb458c26797402e45d01fd6818d7d7e10117
ziyad-bit <ziyad199523@gmail.com>
2026-04-09 16:19:47 +0200

    seed

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -16,20 +16,29 @@ class AdminSeeder extends Seeder
     {
         $faker = Factory::create();
 
-        for ($i = 0; $i < 100; $i++) {
-            $date   = $faker->dateTimeBetween('-5 years');
-            $name   = $faker->name();
+        Admin::create([
+            'name'                => 'ziyad',
+            'email'               => 'ziyad@gmail.com',
+            'bio'                 => $faker->paragraph(),
+            'address'             => $faker->address(),
+            'phone'               => $faker->numberBetween(100000000, 300000000),
+            'password'            => Hash::make('13131313'),
+        ]);
 
-            Admin::insert([
-                'name'                => $name,
-                'email'               => $faker->unique()->safeEmail(),
-                'bio'                 => $faker->paragraph(),
-                'address'             => $faker->address(),
-                'phone'               => $faker->numberBetween(100000000, 300000000),
-                'password'            => Hash::make('13131313'),
-                'created_at'          => $date,
-                'updated_at'          => $date,
-            ]);
-        }
+        // for ($i = 0; $i < 100; $i++) {
+        //     $date   = $faker->dateTimeBetween('-5 years');
+        //     $name   = $faker->name();
+
+        //     Admin::insert([
+        //         'name'                => $name,
+        //         'email'               => $faker->unique()->safeEmail(),
+        //         'bio'                 => $faker->paragraph(),
+        //         'address'             => $faker->address(),
+        //         'phone'               => $faker->numberBetween(100000000, 300000000),
+        //         'password'            => Hash::make('13131313'),
+        //         'created_at'          => $date,
+        //         'updated_at'          => $date,
+        //     ]);
+        // }
     }
 }
