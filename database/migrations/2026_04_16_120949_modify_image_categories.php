86b38d26c483057172b3670aa48fb34a4f680cfb
ziyad-bit <ziyad199523@gmail.com>
2026-04-16 14:15:17 +0200

    edit_image_category

--- database/migrations/2026_04_16_120949_modify_image_categories.php
+++ database/migrations/2026_04_16_120949_modify_image_categories.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('products', function (Blueprint $table) {
+        Schema::table('categories', function (Blueprint $table) {
             $table->string('image', 110)->change();
         });
 
