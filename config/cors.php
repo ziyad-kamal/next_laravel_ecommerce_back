7911d7a43353b612e1e41c7e57400e9f8423f046
ziyad-bit <ziyad199523@gmail.com>
2026-07-02 18:56:26 +0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -19,12 +19,13 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'https://ecocity.info',
-        'https://www.ecocity.info',
-        'http://localhost:7777',      // Most common for Next.js
-        'http://127.0.0.1:7777',
-        'https://next.ecocity.info',
-        'https://www.next.ecocity.info'
+        // 'https://ecocity.info',
+        // 'https://www.ecocity.info',
+        // 'http://localhost:7777',      // Most common for Next.js
+        // 'http://127.0.0.1:7777',
+        // 'https://next.ecocity.info',
+        // 'https://www.next.ecocity.info'
+        '*'
     ],
 
     'allowed_origins_patterns' => [
